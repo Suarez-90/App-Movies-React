e84e95126c1f39581b5d374aa18c5fb4f930744b
Suarez-90 <ssuarezcalanas@gmail.com>
2025-04-10 22:55:26 -0400

    add validation search again and styles images

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import searchMovies from '../services/movies'
+import { useRef } from "react";
 
 
 export function useMovies({search}) {
   const [movies, setMovies]=useState([])
   const [loading, setLoading] = useState(false); 
   const [error, setError] = useState(null);
+  const valueSearch = useRef({search})
 
   
   const getMovies = async () => {
+    if (valueSearch.current=== search) return
     try {
       setLoading(true)
       setError(null)
+      valueSearch.current = search
       const newMovies = await searchMovies({search})
       setMovies(newMovies)      
     } catch (error) {
@@ -20,5 +24,6 @@ export function useMovies({search}) {
       setLoading(false)
     }
   }
+  
   return { movies, loading, getMovies}
 }
